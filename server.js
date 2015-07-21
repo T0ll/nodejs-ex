41c247f37e5109d0c485a6dbbe5c4e8e537f3e90
T0ll <T0ll@users.noreply.github.com>
2015-07-21 19:09:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ var rest_client = require('node-rest-client').Client;
 var client = new rest_client();
 
 
-var openshift_url = "";
-client.get("https://10.100.203.0:8443/api/v1/namespaces/auto-scaling/replicationcontrollers",function(data, response){
-  var tab = Object.keys(data["items"]);
-  openshift_url="https://10.100.203.0:8443/api/v1/namespaces/auto-scaling/replicationcontrollers/"+tab[tab.length-1];
-  console.log(openshift_url);
-});
+var openshift_url = "https://10.100.203.0:8443/api/v1/namespaces/auto-scaling/replicationcontrollers/nodejs-ex-1";
 
 
 function apply_config(url,data){
